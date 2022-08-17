08174379aa26647bc88dfb9968faddc42625b0e5
Utkarsh keshari <80323216+utkarshbanka@users.noreply.github.com>
2022-08-17 23:55:12 +0530

    Add files via upload

---
+++ Dermstore-master/README.md
@@ -0,0 +1,114 @@
+Hello, 
+
+We are a team of four members and together we have cloned the Dermstore website. Basically, it is a website from where you can purchase skin care products.
+
+The name of all the team members are :-
+
+* Ankit Sah
+
+* Kratika Songara
+
+* Manish Singh Chouhan
+
+* Utkarsh Keshari
+
+
+
+We have tried to create as many features as we can of this website. Some of the pages and features which we created are : 
+
+Landing page, log in, signup page, sell page, add to cart, checkout page, functionality to add any product to cart page and displaying its details and total amount, functionality to scroll horizontally on the home page through buttons in various sections, functionality to get the product description if you click its image on all pages except for homepage and functionality to sort the products in some of the pages.
+
+On the landing page, there are many options like a search bar where users can search products by product name or Brand, then there is also an account option where users can log in or signup and beside it, cart option is also there and by clicking on the cart option users can see the products which they have added to the cart. By clicking on any navbar button their respective page will be open (pages which will open are Bestseller, beautyFix and Sale).
+
+
+![Screenshot (621)](https://user-images.githubusercontent.com/100896129/161421629-f3b04afb-a951-4ac1-b7c6-d9a5b4c4f0dc.png)
+
+
+
+
+In this section of landing page, users can see new arrivals products and scroll through them just by clicking the buttons which are present on the left and right side.
+
+![Screenshot (622)](https://user-images.githubusercontent.com/100896129/161421718-6940f5b7-c141-42fe-86d3-c1c3b8f789d9.png)
+
+ 
+
+
+This is the footer part of the landing page where users can know about the company, privacy policy and payment partner, and many more. They can also sign up from the button present in the footer.
+
+  
+![Screenshot (623)](https://user-images.githubusercontent.com/100896129/161421727-767c20c1-d5a1-411f-9845-8fcbf67c08b2.png)
+
+
+
+This is the Bestseller page. Users can go to this page just by clicking on its name in the navigation bar on the landing page. Users can filter products of their own choice also brand option is there to select the best product.
+
+![Screenshot (624)](https://user-images.githubusercontent.com/100896129/161421750-7c6252e7-de83-43b0-a8ef-bae784f5d986.png)
+
+ 
+
+This is Beautifix page, just clicking on beautifix option in the navigation bar users will be redirected to this page. On this page, there are some company names and by clicking any company name users will be redirected to the company page also subscription option is there users can check the subscription policy by clicking the subscription option
+
+![Screenshot (624) (1)](https://user-images.githubusercontent.com/100896129/161421764-7f820d8d-7ddc-4aec-aebe-661d997cb8ca.png)
+
+
+
+
+Whenever sale starts this page will show those products which are on sale. And here users can also filter the products by prices of their own choice. And they can also add the products to the cart just by clicking on Add to Cart button.
+
+![Screenshot (624) (2)](https://user-images.githubusercontent.com/100896129/161421794-defea6e1-4f2d-44c7-8ec5-b45877a4fe3d.png)
+
+ 
+   
+This is the login page by clicking on the account option on the landing page users will be redirected to the login page if the users are existing customers they can login by the same email and password or by Facebook account or google account. If users are new customers then by clicking on continue they will be redirected to the signup page.
+
+
+
+
+![Screenshot (625)](https://user-images.githubusercontent.com/100896129/161421810-ea8ccd98-f927-47b3-b479-16dbe5493000.png)
+
+
+
+This is the signup page, which the user will get if he clicks on the continue button on the account page and new users can create an account here.
+
+
+![Screenshot (626)](https://user-images.githubusercontent.com/100896129/161421819-d8f0c296-a2cd-4a91-a7a1-19a76fa7f444.png)
+
+
+
+
+This is the user's cart page, after clicking on the add to cart option users can see their product on the cart page. To get redirected to this page users need to click on the cart option in the navigation bar. Here users can check the product name, quantity, and total price.
+
+
+![Screenshot (629)](https://user-images.githubusercontent.com/100896129/161421847-d7b96a2e-5ff9-4801-81ef-bc26453776ee.png)
+
+
+
+
+This is the Checkout page, which is the last page of the website. The user will get redirected to this page once he clicks on the checkout securely button on the cart page. The user needs to fill his account details and then after clicking on submit my order button and after clicking he will get an alert saying "Order placed Successfully".
+
+
+![Screenshot (630)](https://user-images.githubusercontent.com/100896129/161421862-450b84df-acf7-4a6f-b173-125d77ad5ef2.png)
+
+
+
+  :- Language Used to make this website.
+
+ * HTML - 35%
+ * JavaScript  - 35%
+ * CSS - 30%
+
+:-  Tool used for collaboration members.
+
+      1. Git Hub for coding
+
+      2. Zoom for communication 
+
+      3. Slack for communication
+
+                                                    
+                                                 THANK YOU!
+
+
+
+
+
---
+++ Dermstore-master/product.js
@@ -0,0 +1,326 @@
+var seller = [
+    {
+        imgPro : "https://static.thcdn.com/images/small/webp//productimg/original/11289609-8744892770898772.jpg",
+        name : "SkinCeuticals C E Ferulic (1 fl. oz.)",
+        print : 166.00
+
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11370303-1074944341868735.jpg",
+        name : "EltaMD UV Clear Broad-Spectrum SPF 46 (1.7 oz.)",
+        print : 37.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12596429-3594871019700125.jpg",
+        name : "SkinMedica TNS Advanced+ Serum (1 oz.)",
+        print : 295.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11588281-8764882139037003.jpg",
+        name : "Sunday Riley GOOD GENES All-In-One Lactic Acid Treatment (1.7 oz. - $175 Value)",
+        print : 122.00 
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11174178-1044909106371632.jpg",
+        name : "Paula's Choice SKIN PERFECTING 2 BHA Liquid Exfoliant (4 fl. oz.)",
+        print : 32.00 
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/small/webp//productimg/original/11291496-1824871578503201.jpg",
+        name : "iS Clinical Cleansing Complex (6 fl. oz.)",
+        print : 45.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11370312-1044938634157826.jpg",
+        name : "EltaMD UV Clear Broad-Spectrum SPF 46 - Tinted (1.7 oz.)",
+        print : 39.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11290635-1804892767902966.jpg",
+        name : "SkinCeuticals Triple Lipid Restore 242 (1.6 fl. oz.)",
+        print : 139.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11291541-5024871574576726.jpg",
+        name : "Obagi Medical Nu-Derm Toner (6.7 fl. oz.)",
+        print : 43.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289682-1284871019539829.jpg",
+        name : "SkinMedica TNS Essential Serum (1 oz.)",
+        print : 281.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12747871-5844866386756265.jpg",
+        name : "Dr Dennis Gross SpectraLite™ FaceWare Pro (1 piece)",
+        print : 435.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11291956-2074871578544985.jpg",
+        name : "iS Clinical Active Serum (1 fl. oz.)",
+        print : 142.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11288881-1144871571568377.jpg",
+        name : "NuFACE Mini (2 piece)",
+        print : 209.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12901956-1634871508329232.jpg",
+        name : "Supergoop!® Unseen Sunscreen SPF 40 1.7 fl. oz.",
+        print : 36.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/10560417-1534868371575033.jpg",
+        name : "RevitaLash Advanced Eyelash Conditioner - 6 Month Supply (0.118 fl. oz.)",
+        print : 150.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289599-4344892770055931.jpg",
+        name : "Earn 500 reward points when purchasing this product as a rewards member*",
+        print : 100.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11574165-2114870996065159.jpg",
+        name : "SkinMedica HA5 Rejuvenating Hydrator (1 fl. oz.)",
+        print :120.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11395385-5474911401472936.jpg",
+        name : "SkinCeuticals Hyaluronic Acid Intensifier (1 fl. oz.)",
+        print : 120.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11370314-1304944342872775.jpg",
+        name : "EltaMD UV Elements Tinted Broad-Spectrum SPF 44 (2 oz.)",
+        print : 36.50
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13547804-2074932747456540.jpg",
+        name : "SkinCeuticals Anti-Aging Radiance Duo Worth $296.00",
+        print : 296.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11290631-5864870995720591.jpg",
+        name : "SkinMedica HA5 Rejuvenating Hydrator (2 oz.)",
+        print : 178.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597166-1134871572767741.jpg",
+        name : "Olaplex No. 7 Bonding Oil (1 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11535233-1444892768930832.jpg",
+        name : "SkinCeuticals Purifying Cleanser (6.8 fl. oz.)",
+        print : 35
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11574165-2114870996065159.jpg",
+        name : "SkinMedica HA5 Rejuvenating Hydrator (1 fl. oz.)",
+        print :120.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11395385-5474911401472936.jpg",
+        name : "SkinCeuticals Hyaluronic Acid Intensifier (1 fl. oz.)",
+        print : 120.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+    {
+        imgPro : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        name : "Olaplex No. 4 Bond Maintenance Shampoo (8.5 fl. oz.)",
+        print : 28.00
+    },
+
+]
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("load" ,function(){
+    Seller(seller);
+});
+
+
+function sortbyprice(){
+    //var select = document.querySelector("#price").value;
+
+    var selection = document.querySelector("#price").value;
+
+    console.log(selection)
+
+    if(selection=="select"){
+        Seller(seller);
+    }
+
+    if(selection == "LTH"){
+        seller.sort(function(a,b){
+            return a.print - b.print;
+        })
+        Seller(seller);
+    }
+
+    if(selection == "HTL"){
+        seller.sort(function(a,b){
+            return b.print - a.print;
+        })
+        Seller(seller);
+    }
+}
+
+
+function Seller(seller){
+    document.querySelector("#utkk").innerText = "";
+
+
+
+
+
+
+
+seller.map(function(ele){
+
+    var box = document.createElement("div");
+
+    var img = document.createElement("img");
+    img.src = ele.imgPro;
+    img.setAttribute("class","image");
+
+    var name = document.createElement("p");
+    name.innerText = ele.name;
+    name.style.textAlign="center";
+
+    var print = document.createElement("h3");
+    print.innerText = ele.print;
+    print.style.textAlign= "center";
+
+    var btn = document.createElement("button");
+    btn.innerText = "Add to Cart";
+    btn.setAttribute("class","btn-btn")
+
+    box.append(img,name,print,btn)
+    document.querySelector("#utkk").append(box)
+
+    btn.addEventListener("click",function(){
+        product(ele);
+    })
+    img.addEventListener("click",function(){
+        productd(ele)
+    })
+});
+}
+
+
+var arr_product = JSON.parse(localStorage.getItem("product_details")) || []
+
+function product(ele){
+   console.log(ele)
+   var objproduct={
+    img:ele.imgPro,
+    name:ele.name,
+    price:ele.print,
+}
+   arr_product.push(objproduct);
+   localStorage.setItem("product_details",JSON.stringify(arr_product))
+}
+
+var detaq = JSON.parse(localStorage.getItem("detaq")) || []
+function productd(ele)
+{
+    console.log(ele)
+   var onepage = {
+       img:ele.imgPro,
+       name:ele.name,
+       price:ele.print,
+   }
+   detaq.push(onepage);
+   localStorage.setItem("care", JSON.stringify(detaq))
+   window.location.href="productde.html";
+}
